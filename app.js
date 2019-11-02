4f20cd54e287a326be40a45e18a31fe533176a98
Scott Roberson <srkazan23@gmail.com>
2019-11-02 14:20:32 -0500

    added delete & filter functionality

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ loadEventListeners();
 function loadEventListeners(){
   //Add task event
   form.addEventListener('submit',addTask);
+  //Remove task event
+  taskList.addEventListener('click',removeTask);
+  //Clear task event
+  clearBtn.addEventListener('click',clearTasks);
+  //Filter tasks event
+  filter.addEventListener('keyup',filterTasks);
+
 }
 
 //Add Task
@@ -42,4 +49,40 @@ function addTask(e){
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+//Remove Task
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')){
+   
+   if(confirm('Are You Sure?')){
+    e.target.parentElement.parentElement.remove();
+   } 
+  }
+  
+}
+
+//Clear tasks
+function clearTasks(){
+  //taskList.innerHTML='';
+
+  //Faster
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+//Filter tasks
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLocaleLowerCase().indexOf(text)!= -1){
+      task.style.display = 'block';
+    }else{
+      task.style.display = 'none';
+    }
+
+  });
 }
